6aa1723689ce407fb999e2aa55125c4505fb03fc
Dipesh Khanal <dipeshkhanal79@gmail.com>
2025-08-21 23:28:26 +0545

    ref: updated api url

--- src/config/logarc.php
+++ src/config/logarc.php
@@ -2,5 +2,5 @@
 
 return [
     'project_key' => env('LOGARC_PROJECT_KEY'),
-    'endpoint' => env('LOGARC_ENDPOINT','https://logarc.khanaldipesh.com.np/api'),
+    'endpoint' => env('LOGARC_ENDPOINT','https://logarc.com/api'),
 ];
